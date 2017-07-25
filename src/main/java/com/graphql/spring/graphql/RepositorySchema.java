830d6b0ad8620ce9d7ba107049ca8f814278e34a
Krzysztof Rzymkowski <krzysztof.rzymkowski@lingaro.com>
2017-07-25 15:10:50 +0200

    createRawGraphQLSchema

--- src/main/java/com/graphql/spring/graphql/RepositorySchema.java
+++ src/main/java/com/graphql/spring/graphql/RepositorySchema.java
@@ -2,14 +2,20 @@ package com.graphql.spring.graphql;
 
 import graphql.ExecutionResult;
 import graphql.GraphQL;
+import graphql.Scalars;
 import graphql.annotations.GraphQLAnnotations;
+import graphql.schema.GraphQLObjectType;
 import graphql.schema.GraphQLSchema;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Optional;
 import java.util.regex.Pattern;
+
+import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition;
+import static graphql.schema.GraphQLObjectType.newObject;
 
 @Component
 public class RepositorySchema {
@@ -23,12 +29,28 @@ public class RepositorySchema {
             throws IllegalAccessException, NoSuchMethodException, InstantiationException {
         this.queryRoot = query;
         this.mutationsRoot = mutations;
-        this.graphQL = createGraphQLSchema();
+        this.graphQL = createRawGraphQLSchema();
     }
 
     public ExecutionResult execute(String query, String operationName, Map<String, Object> variables) {
         Object root = isMutation(query, operationName) ? mutationsRoot : queryRoot;
         return graphQL.execute(query, operationName, root, variables);
+    }
+
+    private GraphQL createRawGraphQLSchema() throws IllegalAccessException, InstantiationException, NoSuchMethodException {
+        GraphQLObjectType root = newObject()
+                .name("Root")
+                .field(newFieldDefinition()
+                        .name("info")
+                        .type(Scalars.GraphQLString)
+                        .dataFetcher(env -> LocalDateTime.now().toString())
+                        .build())
+                .build();
+        GraphQLSchema schema = GraphQLSchema.newSchema()
+                .query(root)
+                .build();
+        return GraphQL.newGraphQL(schema)
+                .build();
     }
 
     private GraphQL createGraphQLSchema() throws IllegalAccessException, InstantiationException, NoSuchMethodException {
